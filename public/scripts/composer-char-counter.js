a87afffef757e706d70adbb7884a7525307c4b7a
Penny Clark <respect.cactus@gmail.com>
2021-03-24 01:08:29 +0000

    updated tweet text handler

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,16 +4,18 @@ $(document).ready(function() {
   // --- our code goes here ---
 
 
-  $("#tweet-text").on('input', function () {
-    const charLength = $(this).val().length;
-    const maxChar = 140
-    console.log(maxChar-charLength);
-  })
+  $("#tweet-text").on('keyup', onKeyUp); 
 
 });
 
-// const countingChars = function() {
-//   const charLength = $(this).val().length;
-//   const maxChar = 140
-//   console.log(maxChar-charLength);
-// }
+const onKeyUp = function () {
+  const charLength = $(this).val().length;
+  const maxChar = 140
+  console.log(maxChar-charLength); 
+};
+
+const countingChars = function() {
+  const charLength = $(this).length;
+  const maxChar = 140
+  console.log(maxChar-charLength);
+}
